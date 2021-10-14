35f5bc5413f684b5733926a41fb9e999d322f33c
oje-edu <thorsten.oje@gfn.education>
2021-10-14 15:03:35 +0200

    typos

--- client/src/components/characters/CharacterItem.js
+++ client/src/components/characters/CharacterItem.js
@@ -3,7 +3,7 @@ const CharacterItem = ({ item }) => {
     <div className="card">
       <div className="card-inner">
         <div className="card-front">
-          <img src={item.img} alt="" />
+          <img src={item.image} alt="" />
         </div>
         <div className="card-back">
           <h1>{item.firstname}</h1>
